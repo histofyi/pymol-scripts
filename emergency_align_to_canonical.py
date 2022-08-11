81b945e25b407b2d35962d14ea96d372cef62c4b
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-08-11 12:54:55 +0100

    Added a script to align a single/few structures.
    #TODO look at whole pipeline, make it easy to do a step on a single or multiple structures

---
+++ emergency_align_to_canonical.py
@@ -0,0 +1,26 @@
+from pymol import cmd
+
+def align_to_canonical(mhc_class:str, pdb_code:str, assembly_id:int):
+    print ('-----')
+    print (pdb_code)
+    cmd.load('canonical/cannonical_class_i_1hhk.pdb', quiet=1)
+    cmd.load(f'{pdb_code}.pdb', quiet=1)
+
+    align = cmd.cealign('cannonical_class_i_1hhk',pdb_code)
+    align['rmsd'] = align['RMSD']
+    del align['RMSD']
+    cmd.delete('cannonical_class_i_1hhk')
+    print (align['rmsd'])
+    files = {}
+    file_types = ['pdb', 'cif']
+    for file_type in file_types:
+        file_key = f'{pdb_code}_aligned.{file_type}'
+        file_name = f'{file_key}'
+        cmd.save(file_name)
+        files[f'{file_type}_file_key'] =  file_key
+    return align, files
+
+
+
+for pdb_code in ['1mhe_1', '1mhe_2']:
+    align_to_canonical('class_i', pdb_code, 1)
